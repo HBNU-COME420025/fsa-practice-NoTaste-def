d43c4c59766f43d16c7637ac7f19a3ccf44e4bb8
NoTaste-def <ddrnjs1245@gmail.com>
2025-04-08 11:01:01 +0900

    [2025-04-08] - 가상환경 세팅 및 상태천이 예제 구현

--- 00FiniteStateAutomata/fsa_example.py
+++ 00FiniteStateAutomata/fsa_example.py
@@ -1,29 +1,49 @@
-import sys
-sys.path.append('00FiniteStateAutomata/libs') # 상대경로는 실행위치 기준이므로 그때그때 수정 요망.
-
 from pyevsim import BehaviorModelExecutor, SystemSimulator, Infinite
 import datetime
 
 class PEx(BehaviorModelExecutor):
     def __init__(self, instance_time, destruct_time, name, engine_name):
         BehaviorModelExecutor.__init__(self, instance_time, destruct_time, name, engine_name)
-        self.init_state("Wait")
-        self.insert_state("Wait", Infinite)
-        self.insert_state("Check", Infinite)
+        self.init_state("q0")
+        self.insert_state("q0", Infinite)
+        self.insert_state("q1", Infinite)
+        self.insert_state("invalid", Infinite)
 
         self.insert_input_port("event")
+        
+        self.alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789"
+        self.alpha = "abcdefghijklmnopqrstuvwxyz"
 
     def ext_trans(self,port, msg):
         if port == "event":
-            print(msg.retrieve()[0])
-            #self._cur_state = "check"
+            event = msg.retrieve()[0]
+            print(event)
+            if self._cur_state == "q0":
+                if event in self.alpha:
+                    print("ACCEPT")
+                    self._cur_state = "q1"
+                else:
+                    print("NOT ACCEPT")
+                    self._cur_state = "invalid"
+            elif self._cur_state == "q1":
+                if event in self.alphanumeric:
+                    print("ACCEPT")
+                    self._cur_state = "q1"
+                else:
+                    print("NOT ACCEPT")
+                    self._cur_state = "invalid"
+                
 
     def output(self):
         return None
         
     def int_trans(self):
-        if self._cur_state == "check":
-            self._cur_state = "check"
+        if self._cur_state == "q1":
+            self._cur_state = "q1"
+        elif self._cur_state == "q0":
+            self._cur_state = "q0"
+        else:
+            self._cur_state = "invalid"
 
 
 ss = SystemSimulator()
@@ -35,7 +55,9 @@ ss.get_engine("first").register_entity(gen)
 
 ss.get_engine("first").coupling_relation(None, "event", gen, "event")
 
-event_string = "abc"
+event_string = "qwer1234"
 for alpha in event_string:
     ss.get_engine("first").insert_external_event("event", alpha)
     ss.get_engine("first").simulate(1)
+
+
